26415edad612ce0e976081d39bcfcb020803bbb6
martinzandoli <64982299+martinzandoli@users.noreply.github.com>
2025-10-02 00:02:27 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-module.exports = { reactStrictMode: true }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      // Esto hace que "/" sirva directamente tu imagen
+      { source: '/', destination: '/hero.png' },
+    ];
+  },
+};
+
+module.exports = nextConfig;
