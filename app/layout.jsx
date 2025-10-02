fae48fed5332f493af54196984b0c83b4a333326
martinzandoli <64982299+martinzandoli@users.noreply.github.com>
2025-10-02 01:14:33 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-pastel">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
