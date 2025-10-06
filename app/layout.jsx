50bb0eeb1bcca5d630dcb920980f39d268711d7a
martinzandoli <64982299+martinzandoli@users.noreply.github.com>
2025-10-06 19:28:54 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+
 export const metadata = {
   title: "LIV Energy Water",
   description: "Energía limpia, hidratación real.",
@@ -7,8 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
--     <body className="bg-pastel">{children}</body>
-+     <body className="bg-pastel antialiased text-gray-900">{children}</body>
+      <body className="bg-pastel antialiased text-gray-900">
+        {children}
+      </body>
     </html>
   );
 }
